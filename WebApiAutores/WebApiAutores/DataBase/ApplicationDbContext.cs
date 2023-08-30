dab32fbdc1de9aae94f8126d1fd90eedd5f20051
HugoFajardo1 <hugofajardo1@gmail.com>
2023-08-30 14:20:30 -0300

    appsetting.Development

---
+++ WebApiAutores/WebApiAutores/DataBase/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApiAutores.Entities;
+
+namespace WebApiAutores.DataBase
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public DbSet<Autor> Autores { get; set; }
+    }
+}
