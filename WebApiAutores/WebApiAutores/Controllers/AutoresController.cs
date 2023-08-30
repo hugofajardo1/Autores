1357ce5c06f09cc5c7dd94822e03a15251e7fc9d
HugoFajardo1 <hugofajardo1@gmail.com>
2023-08-30 12:07:09 -0300

    AutoresController y Autor

---
+++ WebApiAutores/WebApiAutores/Controllers/AutoresController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApiAutores.Entities;
+
+namespace WebApiAutores.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AutoresController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<List<Autor>> Get() 
+        {
+            return new List<Autor>()
+            {
+                new Autor(){ Id = 1, Name="Hugo" },
+                new Autor(){ Id = 2, Name="Jorge" }
+            };
+
+        }
+    }
+}
