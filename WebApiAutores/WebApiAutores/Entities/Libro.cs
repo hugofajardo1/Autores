bcf5ec1e213e4c36f15abbf3b6091b19854bfe2a
HugoFajardo1 <hugofajardo1@gmail.com>
2023-09-21 10:29:01 -0300

    Update Libro.cs

--- WebApiAutores/WebApiAutores/Entities/Libro.cs
+++ WebApiAutores/WebApiAutores/Entities/Libro.cs
@@ -9,5 +9,6 @@ namespace WebApiAutores.Entities
         [StringLength(maximumLength: 100, ErrorMessage = "El campo {0} no debe tener mas de {1} caracteres")]
         public string Title { get; set; }
         public List<Comentario> Comentarios { get; set; }
+        public List<AutorLibro> AutorLibros { get; set;}
     }
 }
