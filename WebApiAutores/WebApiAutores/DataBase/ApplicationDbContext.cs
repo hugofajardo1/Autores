7f3941bac28466a5187ae049ddee0e4699367ecb
HugoFajardo1 <hugofajardo1@gmail.com>
2023-09-22 09:10:56 -0300

    AutorLibro Migracion

--- WebApiAutores/WebApiAutores/DataBase/ApplicationDbContext.cs
+++ WebApiAutores/WebApiAutores/DataBase/ApplicationDbContext.cs
@@ -9,9 +9,15 @@ namespace WebApiAutores.DataBase
         {
 
         }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<AutorLibro>().HasKey(al => new { al.AutorId, al.LibroId });
+        }
 
-        public DbSet<Autor> Autores { get; set; }
+        public DbSet<Autor> Autores { get; set; }   
         public DbSet<Libro> Libros { get; set; }
         public DbSet<Comentario> Comentarios { get; set; }
+        public DbSet<AutorLibro> AutoresLibros { get; set; }
     }
 }
--- WebApiAutores/WebApiAutores/Entities/Autor.cs
+++ WebApiAutores/WebApiAutores/Entities/Autor.cs
@@ -8,5 +8,7 @@ namespace WebApiAutores.Entities
         [Required(ErrorMessage = "El campo {0} es requerido")]
         [StringLength(maximumLength: 100, ErrorMessage = "El campo {0} no debe tener mas de {1} caracteres")]
         public string Name { get; set; }
+        public List<AutorLibro> AutoresLibros { get; set; }
     }
 }
+    
